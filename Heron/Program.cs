63ed6232951ea3e06adebec9e51d436d7070534e
aronmal <aron@mal-noh.de>
2022-02-15 11:49:01 +0100

    Implementet 'IsInRange'

--- Heron/Program.cs
+++ Heron/Program.cs
@@ -8,26 +8,41 @@ namespace Heron
         {
             Console.WriteLine("Dieses Programm verwendet das Heron-Verfahren um die 2. Wurzel eines Radianten zu lösen.");
             Console.WriteLine("- - - - -");
+            Console.WriteLine("");
+            // input = ELib.getLong("Input a number <Input>");
+            // exponent = ELib.getInt("Input exponent <Exponent>");
             Console.Write("Wurzel ziehen aus (Radiant): ");
-            int radiant = int.Parse(Console.ReadLine());
+            var radiant = int.Parse(Console.ReadLine());
             Console.Write("Wie vielte Wurzel?: ");
-            int exponent = int.Parse(Console.ReadLine());
+            var exponent = int.Parse(Console.ReadLine());
             Console.Write("Genauigkeit an Nachkommastellen: ");
-            int precision = int.Parse(Console.ReadLine());
+            var precision = Math.Pow(10, -1 * int.Parse(Console.ReadLine()));
             Console.WriteLine("- - - - -");
             double x = radiant;
             double y = 1;
-            int index = 1;
-            while (Math.Round(x, precision) != Math.Round(y, precision))
+            var index = 1;
+            while (!IsInRange(x, y, precision))
             {
                 Console.WriteLine("Round " + index + ":");
                 index++;
-                Console.WriteLine(" | " + Math.Round(x, precision) + " | " + Math.Round(y, precision) + " | ");
+                Console.WriteLine(" | " + Math.Round(x, 5) + " | " + Math.Round(y, 5) + " | ");
                 x = ((x * (exponent - 1)) + y) / exponent;
                 y = radiant / Math.Pow(x, exponent - 1);
             }
+            // Console.WriteLine($"The {exponent} root is {inputX.ToString(range.ToString())}; TimesRan {timesRan}");
             Console.WriteLine("Final Result:");
             Console.WriteLine(" | " + x + " | " + y + " | ");
+        }
+        private static bool IsInRange(double num1, double num2, double range)
+        {
+            double num;
+            if (num1 > num2)
+            {
+                num = num1 - num2;
+                return num <= range;
+            }
+            num = num2 - num1;
+            return num <= range;
         }
     }
 }
